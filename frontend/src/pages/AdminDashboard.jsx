3f77f26e7bc134254d8d738db16c448b1d24960d
riyasingh49 <riyasingh454247@gmail.com>
2025-08-06 14:06:29 +0530

    qq

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 function AdminDashboard() {
   const [users, setUsers] = useState([]);
