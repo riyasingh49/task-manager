9669e84e35e8f48d8d6c22c6135a58689e489ce4
riyasingh49 <riyasingh454247@gmail.com>
2025-08-06 13:10:29 +0530

    a

--- backend/app.js
+++ backend/app.js
@@ -9,11 +9,10 @@ const app = express();
 
 // Correct CORS setup
 app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
+  origin: 'https://task-manager-frontend.onrender.com',
+  credentials: true
 }));
+
 
 app.use(express.json());
 
